456117245b686602f4efdf866b405b541d1074c3
FC <falcu2345@gmail.com>
2021-11-20 13:11:05 -0500

    singleton pattern on server

--- classes/server.ts
+++ classes/server.ts
@@ -5,17 +5,23 @@ import http from 'http';
 
 export default class Server {
 
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
     public io: socketIO.Server;
     private httpServer: http.Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
         this.httpServer = new http.Server(this.app);
         this.io = new socketIO.Server(this.httpServer, { cors: { origin: true, credentials: true } });
         this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
     }
 
     private escucharSockets() {
