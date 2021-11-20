4a051851a324170012ef0ba77d2f2ff4f17e23f7
FC <falcu2345@gmail.com>
2021-11-19 22:11:01 -0500

    configuring CORS

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Server from './classes/server';
 import { SERVER_PORT } from './global/environment';
 import router from './routes/router';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const server = new Server();
 
@@ -9,6 +10,10 @@ const server = new Server();
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
 
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// Rutas de servicios
 server.app.use('/', router);
 
 server.start(() => {
