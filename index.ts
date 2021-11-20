271dd9ad611879c95928171d9d2054708db20c6e
FC <falcu2345@gmail.com>
2021-11-19 22:06:06 -0500

    receiving parameters in services

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import Server from './classes/server';
 import { SERVER_PORT } from './global/environment';
 import router from './routes/router';
+import bodyParser from 'body-parser';
 
 const server = new Server();
+
+// BodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
 
 server.app.use('/', router);
 
