a0c77f4dbf7f0a9e87553e744927cf93b14772b7
FC <falcu2345@gmail.com>
2021-11-22 01:35:32 -0500

    emit and get active users

--- classes/server.ts
+++ classes/server.ts
@@ -32,17 +32,19 @@ export default class Server {
             // console.log(cliente.id);
 
             // Conectar cliente
-            socket.conectarCliente(cliente);
+            socket.conectarCliente(cliente, this.io);
 
             // Login (configurar usuario)
             socket.configurarUsuario(cliente, this.io);
 
+            // Obtener usuarios activos
+            socket.obtenerUsuarios(cliente, this.io);
+
             // Mensajes
             socket.mensaje(cliente, this.io);
 
-
             // Desconectar
-            socket.desconectar(cliente);
+            socket.desconectar(cliente, this.io);
 
         });
     }
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -5,15 +5,16 @@ import { Usuario } from '../classes/usuario';
 
 export const usuariosConectados = new UsuariosLista();
 
-export const conectarCliente = (cliente: Socket) => {
+export const conectarCliente = (cliente: Socket, io: socketIO.Server) => {
     const usuario = new Usuario(cliente.id);
     usuariosConectados.agregar(usuario);
 }
 
-export const desconectar = (cliente: Socket) => {
+export const desconectar = (cliente: Socket, io: socketIO.Server) => {
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
         usuariosConectados.borrarUsuario(cliente.id);
+        io.emit('usuarios-activos', usuariosConectados.getLista());
     });
 }
 
@@ -30,9 +31,22 @@ export const configurarUsuario = (cliente: Socket, io: socketIO.Server) => {
     cliente.on('configurar-usuario', (payload: { nombre: string }, callback: Function) => {
         // console.log('Configurando usuario: ', payload.nombre);
         usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
+        io.emit('usuarios-activos', usuariosConectados.getLista());
         callback({
             ok: true,
             mensaje: `Usuario ${payload.nombre} configurado`
         });
     });
 }
+
+// Obtener usuarios
+export const obtenerUsuarios = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('obtener-usuarios', () => {
+        // Devolver el listado de usuarios al usuario que lo solicita
+        // io.in(cliente.id).emit('usuarios-activos', usuariosConectados.getLista());
+        io.to(cliente.id).emit('usuarios-activos', usuariosConectados.getLista());
+        // Devolver el listado de usuarios a todo el mundo
+        // io.emit('usuarios-activos', usuariosConectados.getLista());
+    });
+}
+
