16698238a1a6a3195c6864c66f22f813adafa0a3
FC <falcu2345@gmail.com>
2021-11-21 22:19:56 -0500

    save, update name and delete user

--- classes/server.ts
+++ classes/server.ts
@@ -28,13 +28,18 @@ export default class Server {
     private escucharSockets() {
         console.log('Escuchando conexiones - Sockets');
         this.io.on('connection', cliente => {
-            console.log('Cliente conectado');
+            // console.log('Cliente conectado');
+            // console.log(cliente.id);
+
+            // Conectar cliente
+            socket.conectarCliente(cliente);
+
+            // Login (configurar usuario)
+            socket.configurarUsuario(cliente, this.io);
 
             // Mensajes
             socket.mensaje(cliente, this.io);
 
-            // Login (configurar usuario)
-            socket.configurarUsuario(cliente, this.io);
 
             // Desconectar
             socket.desconectar(cliente);
